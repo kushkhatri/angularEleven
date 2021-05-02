b57ae2ab79f5744e5c01f71bb2ae5438f28776fb
kush khatri <kushkhatr88@gmail.com>
2021-05-02 14:22:15 +0530

    update files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  
+
     title = 'angular-one-one';
     // that is parent component
     user = "";
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,13 +10,21 @@ import { AdminComponent } from './admin/admin.component';
 import { TableComponent } from './table/table.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatTableModule} from '@angular/material/table';
+import {MatButtonModule} from '@angular/material/button';
+import {MatDividerModule} from '@angular/material/divider'
+import {MatIconModule} from '@angular/material/Icon';
+import {MatSidenavModule} from "@angular/material/sidenav";
+
+
+import { ButtonsComponent } from './buttons/buttons.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UsersComponent,
     AdminComponent,
-    TableComponent
+    TableComponent,
+    ButtonsComponent
   ],
   imports: [
     BrowserModule,
@@ -24,7 +32,11 @@ import {MatTableModule} from '@angular/material/table';
       UsersModule,
       FormsModule,
       BrowserAnimationsModule,
-      MatTableModule
+      MatTableModule,
+      MatButtonModule,
+      MatDividerModule,
+      MatIconModule,
+      MatSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
